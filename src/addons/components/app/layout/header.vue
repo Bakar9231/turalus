11ca37b0ed913b12359c19275a02f8c426f060e9
Bakar9231 <mabubakar9231@gmail.com>
2022-07-29 14:58:59 +0000

    Mobile responsive

--- src/addons/components/app/layout/header.vue
+++ src/addons/components/app/layout/header.vue
@@ -3,10 +3,10 @@
 <nav class="headroom header navbar navbar-expand-xl navbar-togglable fixed-top navbar-dark">
 <div class="container">
 <a class="navbar-brand" href="index.html"></a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+<button @click="menuToggle" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
 <span class="navbar-toggler-icon"></span>
 </button>
-<div class="collapse navbar-collapse" id="navbarCollapse">
+<div class="collapse navbar-collapse" id="mainMenu">
 <ul class="navbar-nav m-auto">
 <li class="nav-item">
 <a class="nav-link scroll-link" href="#locations">Locations</a>
@@ -45,29 +45,36 @@ export default {
     window.removeEventListener("scroll", this.handleScroll);
   },
   methods: {
+
+    menuToggle(){
+      console.log("in menu function");
+        let menu = document.querySelector(".navbar-collapse");
+        if(!menu.className.includes("customShow")){
+          console.log("in if");
+          menu.className += " customShow";
+        }
+        else{
+          menu.classList.remove("customShow");
+        }
+    },
+
     task(){
         console.log("Triggered just one time >= 80");
     },
-    task2(){
-        console.log("Triggered task2");
-    },
+
     handleScroll(event) {
       // Any code to be executed when the window is scrolled
       const offsetTop = window.scrollY || 0;
       const percentage = (offsetTop * 100) / document.body.scrollHeight;
       console.log(percentage);
-      let header = document.querySelector(".headroom")
-      if(!header.className.includes('v-toolbar--bgchange')){
-       
-          header.className += " v-toolbar--bgchange";
-        
-      }
+      let header = document.querySelector(".headroom");
 
-      else{
-        this.task2();
+      if(!header.className.includes('v-header--bgchange')){
+          header.className += " v-header--bgchange";
       }
-
-      // Do something with the percentage
+      if(percentage === 0 && header.className.includes('v-header--bgchange')){
+        header.classList.remove("v-header--bgchange");
+      }
     },
   },
 }
